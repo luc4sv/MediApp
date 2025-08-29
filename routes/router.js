281bfa574cca5171cc0f1b7043f0a51f3f5739d9
Lucas Vinícius <74675742+luc4sv@users.noreply.github.com>
2025-08-29 19:17:32 -0300

    Router mapping

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,8 @@
 import { express } from 'express';
+import appointmenteController from '../controllers/appointmentController.js';
+import doctorController from '../controllers/doctorController.js';
+import pacientController from '../controllers/pacientController.js';
+import prescriptionController from '../controllers/prescriptionController.js';
 
 let router = express.Router();
 
@@ -6,5 +10,10 @@ router.get('/', (req, res) => {
   console.log('Hello, World!');
   res.status(200).send({ message: 'deu bom'});
 });
+
+router.use('/', pacientController);
+router.use('/', doctorController);
+router.use('/', appointmenteController);
+router.use('/', prescriptionController);
 
 export default router;  
